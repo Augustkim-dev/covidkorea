7f293b26a34b80f73a1eaa2222743abfcb3ba5e8
August Kim <augustkim.dev@gmail.com>
2021-08-07 21:00:45 +0900

    화면 그림 그리는 중
    바탕색,
    배경 바이러스
    글자들 자리잡기

--- covidkorea/lib/src/app.dart
+++ covidkorea/lib/src/app.dart
@@ -3,38 +3,154 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
 class App extends GetView<CovidStatisticsController> {
-  const App({Key? key}) : super(key: key);
+  App({Key? key}) : super(key: key);
+  late double hearderTopZone;
 
   Widget infoWidget(String title, String value) {
-    return  Padding(
+    return Padding(
       padding: const EdgeInsets.all(15.0),
-      child: Row(children: [
-                Text(title, style: TextStyle(fontWeight: FontWeight.bold, fontSize: 15,),
-                ),
-                Text(" : $value", style: TextStyle(fontSize: 15,),
-                ),
-              ],),
+      child: Row(
+        children: [
+          Text(
+            title,
+            style: TextStyle(
+              fontWeight: FontWeight.bold,
+              fontSize: 15,
+            ),
+          ),
+          Text(
+            " : $value",
+            style: TextStyle(
+              fontSize: 15,
+            ),
+          ),
+        ],
+      ),
     );
   }
 
   @override
   Widget build(BuildContext context) {
+    hearderTopZone = Get.mediaQuery.padding.top + AppBar().preferredSize.height;
     return Scaffold(
       appBar: AppBar(
-        title: Text("코로나 일별 현황"),
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        leading: Icon(
+          Icons.menu,
+          color: Colors.white,
+        ),
+        title: Text(
+          "코로나 일별 현황",
+          style: TextStyle(color: Colors.white),
+        ),
         centerTitle: true,
+        actions: [
+          Container(
+            padding: EdgeInsets.symmetric(horizontal: 15),
+            child: Icon(
+              Icons.notifications,
+              color: Colors.white,
+            ),
+          ),
+        ],
       ),
-      body: Container(
-        padding: const EdgeInsets.all(15),
-        child: Obx(() {
-          var info = controller.covidStatisticsModel.value;
-          return Column(children: [
-              infoWidget("기준일", info.stateDt ?? ''),
-              infoWidget("확진자수", info.decideCnt ?? '' ),
-              
-
-          ],);
-        }),
+      extendBodyBehindAppBar: true,
+      body: Stack(
+        children: [
+          Container(
+            // padding: const EdgeInsets.all(15),
+            // child: Obx(() {
+            //   var info = controller.covidStatisticsModel.value;
+            //   return Column(children: [
+            //       infoWidget("기준일", info.stateDt ?? ''),
+            //       infoWidget("확진자수", info.decideCnt ?? '' ),
+            //   ],);
+            // }),
+            decoration: BoxDecoration(
+              gradient: LinearGradient(
+                begin: Alignment.topRight,
+                end: Alignment.bottomLeft,
+                colors: [
+                  Colors.blue,
+                  Color(0xff3c727c),
+                  Colors.red,
+                ],
+              ),
+            ),
+          ),
+          Positioned(
+            left: -110,
+            top: hearderTopZone + 40,
+            child: Container(
+              child: Image.asset(
+                'assets/covid_img.png',
+                width: Get.size.width * 0.7,
+              ),
+            ),
+          ),
+          Positioned(
+            top: hearderTopZone + 10,
+            left: 0,
+            right: 0,
+            child: Center(
+              child: Container(
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 20, vertical: 5),
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(20),
+                  color: Color(0xff195f68),
+                ),
+                child: Text(
+                  '07.24 00:00 기준',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+            ),
+          ),
+          Positioned(
+            top: hearderTopZone + 60,
+            right: 0,
+            child: Column(
+              children: [
+                Text(
+                  '확진자',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 18,
+                  ),
+                ),
+                Row(
+                  children: [
+                    Container(
+                      width: 20,
+                      height: 20,
+                      color: Colors.red,
+                    ),
+                    Text(
+                      '1,629',
+                      style: TextStyle(
+                        color: Color(0xffcf5f51),
+                        fontWeight: FontWeight.bold,
+                        fontSize: 50,
+                      ),
+                    ),
+                  ],
+                ),
+                Text(
+                  '187,362',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 20,
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
       ),
     );
   }
